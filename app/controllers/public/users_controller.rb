769dde23af42d9a1009fcc3a9ff66f4b21ebac76
EC2 Default User <ec2-user@ip-172-31-31-88.ap-northeast-1.compute.internal>
2023-02-26 15:30:09 +0000

    scale

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -45,7 +45,7 @@ class Public::UsersController < ApplicationController
     
 private
   def user_params
-    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :telephone_number, :is_deleted, :profile_image)
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :telephone_number, :is_deleted, :profile_image, :introduction)
   end
   
 end
